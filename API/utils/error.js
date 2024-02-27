4df3115e00d9103e6e198cd9930e5b3cb91512b9
Abhishek Jha <abhishekkj522001@gmail.com>
2024-02-27 14:36:06 +0530

    Added a middleware and a function to handle error

---
+++ API/utils/error.js
@@ -0,0 +1,9 @@
+// We can customize the error message through this function or controller
+
+
+export const errorHandler = (statusCode, message) => {
+    const error = new Error(message); 
+    error.statusCode = statusCode;
+    error.message = message;
+    return error;
+};
