663e2b3ed8e5eaf6e5d771fd7a450fa64d60b71d
Abhishek Jha <abhishekkj522001@gmail.com>
2025-03-02 15:12:59 +0530

    Added comments to the code and Updated notes

--- API/index.js
+++ API/index.js
@@ -5,7 +5,7 @@ import userRoutes from './routes/user-route.js';
 import authRoutes from './routes/auth-route.js';
 import cookieParser from 'cookie-parser';
 import path from 'path';
-dotenv.config ();
+dotenv.config (); //Initialize dotenv for .end file to access it in index.js
 
 mongoose
   .connect (process.env.MONGO)
@@ -14,7 +14,7 @@ mongoose
   })
   .catch (err => {
     console.log (err);
-  });
+});
 
 const __dirname = path.resolve();
 
@@ -26,7 +26,7 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "client", "dist", "index.html"))
 });
 
-app.use (express.json ());
+app.use (express.json ()); // to automatically parse JSON data from incoming requests
 app.use (cookieParser ());
 
 app.listen (3000, () => {
