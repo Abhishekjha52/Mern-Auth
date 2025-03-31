20b2dcd1af9712409935acab50ef57af46e95e0f
Abhishek Jha <abhishekkj522001@gmail.com>
2025-03-31 16:10:56 +0530

    Updated notes, updated packages, comments written in store,js and index.js

--- API/index.js
+++ API/index.js
@@ -5,11 +5,11 @@ import userRoutes from './routes/user-route.js';
 import authRoutes from './routes/auth-route.js';
 import cookieParser from 'cookie-parser';
 import path from 'path';
-dotenv.config (); //Initialize dotenv for .end file to access it in index.js
+dotenv.config(); //Initialize dotenv for .env file to access it in index.js
 
 // Connect to DB
 mongoose
-  .connect ("mongodb+srv://abhishekkj522001:Abhishekjha@mern-auth-project.o1ok13n.mongodb.net/?retryWrites=true&w=majority&appName=mern-auth-project")
+  .connect (process.env.MONGO)
   .then (() => {
     console.log ('Connected to MONGODB');
   })
@@ -19,12 +19,12 @@ mongoose
 
 const __dirname = path.resolve();
 
-const app = express (); 
+const app = express ();
 
-app.use(express.static(path.join(__dirname, "/client/dist")))
+app.use(express.static(path.join(__dirname, '/client/dist')));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "dist", "index.html"))
+app.get ('*', (req, res) => {
+  res.sendFile (path.join (__dirname, 'client', 'dist', 'index.html'));
 });
 
 app.use (express.json ()); // to automatically parse JSON data from incoming requests
@@ -34,10 +34,16 @@ app.listen (3000, () => {
   console.log ('Server listening on port 3000');
 });
 
+// app.get('/', (req, res)=>{
+//   res.json({
+//     message: "Api is working",
+//   });
+// });
+
 app.use ('/api/user', userRoutes);
 app.use ('/api/auth', authRoutes);
 
-// Error handling
+// Middle ware for Error handling
 app.use ((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || 'Internal Server Error';
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -3,12 +3,12 @@ import userReducer from './user/userSlice.js';
 import {persistReducer, persistStore} from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 
-const rootReducer = combineReducers ({user: userReducer});
+const rootReducer = combineReducers ({user: userReducer}); //combine all reducers into one rootreducer and then use it in persist reducer
 
 const persistConfig = {
   key: 'root',
   version: 1,
-  storage,
+  storage, //this helps to store the user data in browser localstorage
 };
 
 const persistedReducer = persistReducer (persistConfig, rootReducer);
